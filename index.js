ed49a70198b2e93479a273dc2e06c79c0b46e5c0
narithzz <105197206+narithzz@users.noreply.github.com>
2025-06-21 20:13:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Premium QR Code Generator</title>
+        <title>QR Code Generator</title>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
         <style>
             :root {
